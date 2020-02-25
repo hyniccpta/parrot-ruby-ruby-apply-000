0803ad6dabdf745f938d21914997b66f3e17557d
interpreted-keyword-9673 <mel.cantorias@gmail.com>
2020-02-25 09:45:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase)
+def parrot("Squawk!")
   puts phrase
   return phrase
-  
 end
-parrot("Squawk!")
